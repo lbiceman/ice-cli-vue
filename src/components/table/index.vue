854091d7963efa84c825f8f19c60ce9137797dfe
liweibing <lbiceman@126.com>
2022-09-29 15:33:41 +0800

    feat: 添加table组件（未完成）

--- src/components/table/index.vue
+++ src/components/table/index.vue
@@ -4,7 +4,6 @@ export default { name: "IceTable" };
 
 <script lang="ts" setup>
 import { computed, ref } from "vue";
-import type { PaginationConfig } from "ant-design-vue/es/pagination";
 import { isFun, isStr, isObj } from "@/utils/index";
 
 const props = defineProps<{
@@ -12,13 +11,6 @@ const props = defineProps<{
 }>();
 
 const tableRef = ref(null);
-
-const hasPagination = computed(() => {
-	const { pageSize, total } = (props.table?.pagination ?? {}) as PaginationConfig;
-	return (total || 0) > (pageSize || 20);
-});
-
-console.log(hasPagination);
 
 const tableProps = computed(() => {
 	return Object.assign({ bordered: true }, props.table ?? {}, {
@@ -31,37 +23,21 @@ const tableProps = computed(() => {
 			showSizeChanger: true,
 			showTotal: (total: number) => `共 ${total} 条记录`,
 			...(props.table?.pagination ?? {})
-		},
-		tableLocal: {
-			filterConfirm: "确定",
-			filterReset: "重置",
-			emptyText: "暂无数据",
-			...(props.table?.locale ?? {})
 		}
 	});
 });
-
-const isSlot = (slot?: any | string) => isObj(slot) || isFun(slot) || isStr(slot);
 </script>
 
 <template>
-	<div class="search-table-wrap">
-		<div ref="tableRef" class="search-table">
-			<a-table v-bind="tableProps">
-				<template v-if="table?.slots?.expandIcon"> </template>
-				<template #bodyCell="{ text, column }">
-					<template v-if="isSlot(column.slot)"> </template>
-					<template v-else>
-						{{ text }}
-					</template>
-				</template>
-			</a-table>
+	<div class="ice-table-wrap">
+		<div ref="tableRef" class="ice-table">
+			<a-table v-bind="tableProps"> </a-table>
 		</div>
 	</div>
 </template>
 
 <style lang="less" scoped>
-.search-table-wrap {
+.ice-table-wrap {
 	flex: 1;
 	overflow: hidden;
 	padding: 12px;
@@ -71,7 +47,7 @@ const isSlot = (slot?: any | string) => isObj(slot) || isFun(slot) || isStr(slot
 	display: flex;
 	flex-direction: column;
 
-	.search-table {
+	.ice-table {
 		flex: 1;
 		overflow: hidden;
 		margin-top: 12px;
@@ -79,19 +55,6 @@ const isSlot = (slot?: any | string) => isObj(slot) || isFun(slot) || isStr(slot
 
 		:deep(.ant-table-wrapper) {
 			height: 100%;
-			// .ant-spin-nested-loading,
-			// .ant-spin-container {
-			// 	height: 100%;
-			// }
-			// .ant-spin-container {
-			// 	height: 100%;
-			// 	display: flex;
-			// 	flex-direction: column;
-			// }
-			// .ant-table {
-			// 	flex: 1;
-			// 	overflow: auto;
-			// }
 		}
 	}
 }
